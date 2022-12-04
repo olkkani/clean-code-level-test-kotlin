ea7c9582edb6bf33dc9c1aace8d2f4f9dc87dc15
chanjin_ahn <olkkani@gmail.com>
2022-12-04 16:07:30 +0900

    :arrow_down: Change springboot version 3.0.0 -> 2.7.5
    springboot-web 하위의 commonmultipartfile 라이브러리 사용을 위해 변경

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,10 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.springframework.boot") version "3.0.0"
+	id("org.springframework.boot") version "2.7.5"
 	id("io.spring.dependency-management") version "1.1.0"
 	id("io.freefair.lombok") version "6.5.1"
+
 	kotlin("jvm") version "1.7.21"
 	kotlin("plugin.spring") version "1.7.21"
 }
